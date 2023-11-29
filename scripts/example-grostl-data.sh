a728a3084b31ea30e8c7a949a3fa5953150cb09c
stas <spolonsky@icloud.com>
2023-11-29 15:09:38 -0500

    view-grostl

--- scripts/example-grostl-data.sh
+++ scripts/example-grostl-data.sh
@@ -1,7 +1,7 @@
-batch_size=32768
+batch_size=65536
 msg_size=8192
-runtime_sec='0.00003253'
-throughput_GiB_sec='7.504'
+runtime_sec='0.000051318'
+throughput_GiB_sec='9.514'
 power_Watt='4.0'
 chip_temp_C='50.0'
 frequency_MHz=300
@@ -9,7 +9,4 @@ project='ingo-hash'
 git_id='2d8acde'
 comment='grostl_256'
 runs_on='vu13p (U250 board)'
-
-
-
 
