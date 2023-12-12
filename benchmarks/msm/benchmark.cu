4b47cf0ef50ad2d15d7f06d440a60358cc9187b0
stas <spolonsky@icloud.com>
2023-12-11 20:28:48 -0500

    msm benchmark

---
+++ benchmarks/msm/benchmark.cu
@@ -0,0 +1,137 @@
+#define CURVE_BN254     1
+#define CURVE_BLS12_381 2
+#define CURVE_BLS12_377 3
+
+#define CURVE CURVE_BLS12_377
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cuda_runtime.h>
+#include <nvml.h>
+#include <benchmark/benchmark.h>
+#include "icicle/primitives/field.cuh"
+#include "icicle/utils/storage.cuh"
+#include "icicle/primitives/projective.cuh"
+
+#include "icicle/appUtils/msm/msm.cu"
+
+#if CURVE == CURVE_BN254
+
+#include "icicle/curves/bn254/curve_config.cuh"    
+using namespace BN254;
+const std::string curve = "BN254";
+
+#elif CURVE == CURVE_BLS12_381
+
+#include "icicle/curves/bls12_381/curve_config.cuh"
+using namespace BLS12_381;
+const std::string curve = "BLS12-381";
+
+#elif CURVE == CURVE_BLS12_377
+
+#include "icicle/curves/bls12_377/curve_config.cuh"
+using namespace BLS12_377;
+const std::string curve = "BLS12-377";
+    
+#endif
+
+const unsigned max_msm_size = 1<<22;
+unsigned bucket_factor = 12;
+
+// on-host data
+scalar_t* scalars;
+affine_t* points;
+projective_t result;
+
+// on-device data
+scalar_t* scalars_d;
+affine_t* points_d;
+projective_t* result_d;
+
+nvmlDevice_t device;
+cudaStream_t stream;
+
+static void BM_msm(benchmark::State& state) {
+  const uint32_t msm_size=state.range(0);  
+  for (auto _ : state) {
+    large_msm<scalar_t, projective_t, affine_t>(scalars_d, points_d, msm_size, result_d, true, false, bucket_factor, stream);
+    cudaDeviceSynchronize();
+  }
+  unsigned int power;
+  nvmlDeviceGetPowerUsage(device, &power);
+  state.counters["PowerUsage"] = int(1.0e-3 * power);
+  unsigned int temperature;
+  nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, &temperature);
+  state.counters["Temperature"] = int(temperature);
+}
+
+BENCHMARK(BM_msm)->MinTime(30.)
+  ->Arg(1<<10)
+  ->Arg(1<<11)
+  ->Arg(1<<12)
+  ->Arg(1<<13)
+  ->Arg(1<<14)
+  ->Arg(1<<15)
+  ->Arg(1<<16)
+  ->Arg(1<<17)
+  ->Arg(1<<18)
+  ->Arg(1<<19)
+  ->Arg(1<<20)
+  ->Arg(1<<21)
+  ->Arg(1<<22);
+
+int main(int argc, char** argv) {
+  cudaDeviceReset();
+  cudaDeviceProp deviceProperties;
+  int deviceId=0;
+  cudaGetDeviceProperties(&deviceProperties, deviceId);
+  std::string gpu_full_name = deviceProperties.name;
+  std::cout << gpu_full_name << std::endl;
+  std::string gpu_name = gpu_full_name;
+  int gpu_clock_mhz = deviceProperties.clockRate/1000.;
+
+  nvmlInit();
+  nvmlDeviceGetHandleByIndex(0, &device);  // for GPU 0
+
+  std::cout << "Setting host data" << std::endl;
+  
+  scalars = (scalar_t*) malloc(sizeof(scalar_t) * max_msm_size);
+  points = (affine_t*)malloc(sizeof(affine_t) * max_msm_size);
+  for (unsigned i = 0; i < max_msm_size; i++) {
+    points[i] = (i % max_msm_size < 10) ? projective_t::to_affine(projective_t::rand_host()) : points[i - 10];
+    scalars[i] = scalar_t::rand_host();
+  }
+
+  std::cout << "Moving data to device" << std::endl;
+
+  cudaMalloc(&scalars_d, sizeof(scalar_t) * max_msm_size);
+  cudaMalloc(&points_d, sizeof(affine_t) * max_msm_size);
+  cudaMalloc(&result_d, sizeof(projective_t));
+  cudaMemcpy(scalars_d, scalars, sizeof(scalar_t) * max_msm_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(points_d, points, sizeof(affine_t) * max_msm_size, cudaMemcpyHostToDevice);
+
+
+  std::cout << "Running benchmark" << std::endl;
+
+  cudaStreamCreate(&stream);
+
+  // Run all benchmarks 
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark::AddCustomContext("team", "Ingonyama");
+  ::benchmark::AddCustomContext("project", "Icicle");
+  ::benchmark::AddCustomContext("runs_on", gpu_name);
+  ::benchmark::AddCustomContext("frequency_MHz", std::to_string(gpu_clock_mhz));
+  ::benchmark::AddCustomContext("uses", curve);
+  ::benchmark::AddCustomContext("comment", "on-device API");
+  ::benchmark::AddCustomContext("coefficient_C", std::to_string(bucket_factor));
+  ::benchmark::RunSpecifiedBenchmarks();
+
+  cudaFree(scalars_d);
+  cudaFree(points_d);
+  cudaFree(result_d);
+  free(scalars);
+  free(points);
+  return 0;
+}
---
+++ benchmarks/msm/run.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# Exit immediately on error
+set -e
+
+DB_HOST=${INGO_BENCHMARKS_DB_HOST}
+DB_PORT=${INGO_BENCHMARKS_DB_PORT}
+DB_NAME=${INGO_BENCHMARKS_DB_NAME}
+DB_USER=${INGO_BENCHMARKS_DB_USER}
+DB_PASS=${INGO_BENCHMARKS_DB_PASS}
+
+
+# testing this Icicle version
+git_id=$(cd /icicle && git rev-parse --short HEAD)
+echo "Icicle GitID: $git_id"
+
+echo "Running the benchmarks and capturing the output in the file benchmark.json"
+/icicle-benchmark/build/benchmark --benchmark_time_unit=s  --benchmark_out_format=json --benchmark_out=benchmark.json
+
+json_data=$(<benchmark.json)
+
+echo "Extracting context..."
+
+team=$(jq -r '.context.team' benchmark.json)
+echo "Team: $team"
+
+project=$(jq -r '.context.project' benchmark.json)
+echo "Project: $project"
+
+runs_on=$(jq -r '.context.runs_on' benchmark.json)
+echo "Runs on: $runs_on"
+
+frequency_MHz=$(jq -r '.context.frequency_MHz' benchmark.json)
+echo "Frequency_MHz: $frequency_MHz"
+
+uses=$(jq -r '.context.uses' benchmark.json)
+echo "Uses: $uses"
+
+coefficient_C=$(jq -r '.context.coefficient_C ' benchmark.json)
+echo "coefficient_C: $coefficient_C"
+
+#vector_size=$(jq -r '.context.vector_size' benchmark.json)
+#echo "Vector Size: $vector_size"
+
+comment=$(jq -r '.context.comment' benchmark.json)
+echo "Comment: $comment"
+
+test_timestamp=$(jq -r '.context.date' benchmark.json)
+#test_timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Timestamp: $test_timestamp"
+
+echo "Extracting benchmarks..."
+
+nof_benchmarks=$(echo "$json_data" | jq '.benchmarks | length')
+echo "Number of benchmarks: $nof_benchmarks"
+
+rm --force benchmark.sql
+for ((nof_benchmark = 0; nof_benchmark < nof_benchmarks; nof_benchmark++)); do
+  echo "Benchmark $nof_benchmark"
+  
+  benchmark=$(jq ".benchmarks[$nof_benchmark]" benchmark.json)
+  # echo $benchmark
+
+  benchmark_name=$(echo "$benchmark" | jq '.name')
+  echo "Complete benchmark name: $benchmark_name"
+
+  # Arguments: the complete benchmark name ends with "/"-separated arguments
+
+  vector_size=$(echo "$benchmark_name" | cut -d'/' -f2)
+  echo "Vector size: $vector_size"
+    
+  runtime_sec=$(echo "$benchmark" | jq '.real_time')
+  echo "Run time (sec): $runtime_sec"
+    
+  batch_size=$(echo "$benchmark" | jq '.iterations')
+  echo "Batch size: $batch_size"
+
+  power_Watt=$(echo "$benchmark" | jq '.PowerUsage')
+  echo "Power (Watt): $power_Watt"
+
+  chip_temp_C=$(echo "$benchmark" | jq '.Temperature')
+  echo "Chip temperature (C): $chip_temp_C"
+
+  QUERY="INSERT INTO msm_benchmark (
+  team,
+  project,
+  test_timestamp,
+  git_id,
+  frequency_MHz,
+  vector_size,
+  coefficient_C,
+  batch_size,
+  runtime_sec,
+  power_Watt,
+  chip_temp_C,
+  comment,
+  runs_on,
+  uses
+)
+VALUES (
+  '$team',
+  '$project',
+  '$test_timestamp',
+  '$git_id',
+  $frequency_MHz,
+  $vector_size,
+  $coefficient_C,
+  $batch_size,
+  $runtime_sec,
+  $power_Watt,
+  $chip_temp_C,
+  '$comment',
+  (SELECT id FROM hw_platform WHERE device = '$runs_on'),
+  (SELECT id FROM finite_field WHERE name = '$uses')
+);
+"
+  echo "$QUERY" >> benchmark.sql
+
+  # Execute the psql command to insert the row into the add_benchmark table
+  # PGPASSWORD=$DB_PASS psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c $QUERY
+
+done
+
+
+#QUERY="SELECT * FROM add_benchmark ORDER BY id DESC LIMIT 10;"
+#PGPASSWORD=$DB_PASS psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c "$QUERY"
+
+exit
+
+
+
+
