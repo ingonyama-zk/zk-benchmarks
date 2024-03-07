9e7a78ca52952d7c45077460185cfe862037c08f
stas <spolonsky@icloud.com>
2024-03-06 20:58:21 -0500

    debug msm benchmark

--- benchmarks/rust/msm/benches/my_benchmark.rs
+++ benchmarks/rust/msm/benches/my_benchmark.rs
@@ -1,20 +1,20 @@
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{criterion_group, Criterion, BenchmarkId};
 use criterion::black_box; // Ensures computations are not optimized away
-use icicle_bn254::curve::{CurveCfg, G1Affine, G1Projective, G2CurveCfg, G2Projective, ScalarCfg, ScalarField};
+use icicle_bn254::curve::{CurveCfg, G1Affine, G1Projective, G2CurveCfg, G2Affine, G2Projective, ScalarCfg, ScalarField};
 use icicle_core::curve::Curve;
 use icicle_core::traits::GenerateRandom;
 use icicle_core::msm;
 use icicle_cuda_runtime::stream::CudaStream;
 use icicle_cuda_runtime::memory::HostOrDeviceSlice;
 use zkbench::{MsmBenchmark,git_id,gpu_name};
-use std::fs;
-use std::fs::OpenOptions;
+// use std::fs;
+// use std::fs::OpenOptions;
 use std::fs::File;
-use std::io::Write;
+// use std::io::Write;
 use std::env;
 use chrono::{Utc, DateTime, NaiveDateTime};
 
-// on-host data
+// on-host G1
 fn msm1(points: &[G1Affine], scalars: &[ScalarField]) -> i32 {
     let size  = points.len();
     let p = HostOrDeviceSlice::Host(points[..size].to_vec());
@@ -31,14 +31,36 @@ fn msm1(points: &[G1Affine], scalars: &[ScalarField]) -> i32 {
     1
 }
 
-// The benchmark function
+// on-host G2
+fn msm2(points: &[G2Affine], scalars: &[ScalarField]) -> i32 {
+    let size  = points.len();
+    let p = HostOrDeviceSlice::Host(points[..size].to_vec());
+    let s = HostOrDeviceSlice::Host(scalars[..size].to_vec());
+    let mut msm_results: HostOrDeviceSlice<'_, G2Projective> = HostOrDeviceSlice::cuda_malloc(1).unwrap();
+    let stream = CudaStream::create().unwrap();
+    let mut cfg = msm::MSMConfig::default();
+    cfg.ctx.stream = &stream;
+    cfg.is_async = true;
+    msm::msm(&s, &p, &cfg, &mut msm_results).unwrap();
+    stream.synchronize().unwrap();
+    // msm_results.copy_to_host(&mut msm_host_result[..]).unwrap();
+    stream.destroy().unwrap();
+    1
+}
+
+
+// The benchmark function: on-host G1
 fn benchmark1(c: &mut Criterion) {
-    let mut group = c.benchmark_group("On-host data");
-    group.sample_size(10); // Default is 100
+    let mut group = c.benchmark_group("rust:on-host-data:G1");
+    group.sample_size(10);
 
-    let sizes = [10, 100];
-
+    // let sizes = [1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824];
+    let sizes = [65536];
+    group.measurement_time(std::time::Duration::from_secs(158));
     for &size in &sizes {
+        // group.throughput(Throughput::Bytes(*size as u64));
+        group.sample_size(10);
+        group.measurement_time(std::time::Duration::from_secs(158));
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
             b.iter_batched_ref(
                 || {  
@@ -56,22 +78,24 @@ fn benchmark1(c: &mut Criterion) {
     group.finish();
 }
 
+// The benchmark function: on-host G2
 fn benchmark2(c: &mut Criterion) {
-    let mut group = c.benchmark_group("On-device data");
-    group.sample_size(10); // Default is 100
-
-    let sizes = [10, 100];
+    let mut group = c.benchmark_group("rust:on-host-data:G2");
+    group.sample_size(10);
+    
+    // let sizes = [1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824];
+    let sizes = [65536];
 
     for &size in &sizes {
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
             b.iter_batched_ref(
                 || {  
-                    let points = CurveCfg::generate_random_affine_points(size);
+                    let points = G2CurveCfg::generate_random_affine_points(size);
                     let scalars = ScalarCfg::generate_random(size);
                     (points, scalars)
                 },
                 |(points, scalars)| {
-                    black_box(msm1(&points, &scalars))
+                    black_box(msm2(&points, &scalars))
                 },
                 criterion::BatchSize::SmallInput, // Adjust based on the relative expense of the setup code
             );
@@ -97,19 +121,19 @@ fn main() {
     m.uses = "BN254".to_string();
     let repository_path = env::var("BENCHMARK_REPO").expect("BENCHMARK_REPO must be set");
     m.git_id = git_id(&repository_path);
-    m.frequency_mhz = 0;
+    m.frequency_mhz = 0; // TODO: get the frequency from the system
     m.vector_size = 0;
     m.batch_size = 1;
-    let device = gpu_name(0);
-    m.runs_on = device;
+    // let device = gpu_name(0);
+    m.runs_on = gpu_name(0);
 
     println!("Running group1 benchmarks...");
-    m.comment = "junk".to_string();
+    // m.comment = "junk".to_string();
     metadata.push(m.clone());
     group1();
     
     println!("Running group2 benchmarks...");
-    m.comment = "junk2".to_string();
+    // m.comment = "junk2".to_string();
     metadata.push(m.clone());
     group2();
 
--- zkbench/src/main.rs
+++ zkbench/src/main.rs
@@ -82,61 +82,51 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     // // Wait for the command to complete
     // let _result = child.wait()?;
 
-    // let metadata_path = "/tmp/metadata.json";
+    let metadata_path = "../benchmarks/rust/msm/metadata.json";
+    // read metadata json into a vector of MsmBenchmark
+    let metadata_file = File::open(metadata_path)?;
+    let metadata: Vec<MsmBenchmark> = serde_json::from_reader(metadata_file)?;
+    println!("{:#?}", metadata[0]);
     // let metadata_json = fs::read_to_string(metadata_path).expect("Failed to read the file");
     // let mut meta: MsmBenchmark = serde_json::from_str(&metadata_json.to_string()).expect("Deserialization failed");
     // println!("Metadata {:?}",meta);
-
+    let connect_result = PgPool::connect(&database_url).await; 
+    let pool = connect_result.unwrap();
     let data_path = Path::new("../benchmarks/rust/msm/criterion.json");
     let data_file = File::open(&data_path)?; 
     let reader = io::BufReader::new(data_file);
-
+    let mut nof_group : usize = 0;
+    let mut nof_benchmark: i32 = 0;
     for line in reader.lines() {
         let line = line?;
         let obj: Result<DataTypes> = from_str(&line);
         match obj {
             Ok(DataTypes::P(performance)) => {
-                println!("Performance: {:?}", performance);
+                println!("Group {:?} Benchmark {:?}", nof_group, nof_benchmark);
+                let id_parts: Vec<&str> = performance.benchmark_id.split('/').collect();
+                let benchmark_group: &str = id_parts[0];
+                let vector_size: i64 = id_parts[1].parse().unwrap();
+                let runtime = normalize_runtime(performance.typical);
+                // println!("Comment {:#?} vector {:#?} runtime_sec {:#?}", benchmark_group, vector_size, runtime);
+                let mut b = metadata[nof_group].clone();
+                b.vector_size = vector_size;
+                b.runtime_sec = Some(runtime);
+                b.comment = "junk ".to_string() + benchmark_group;
+                println!("{:#?}", b);
+                let msm_id = zkbench::add_msm(&pool, b).await;
+                println!("Added new msm with id {:?}", msm_id);
+                nof_benchmark += 1;
             }
             Ok(DataTypes::G(group)) => {
-                println!("Group: {:?}", group);
+                nof_group += 1;
+                nof_benchmark = 0;
+                // println!("Moving to next group: {:?}", nof_group);
             }
             Err(e) => {
                 eprintln!("Error deserializing Performance: {}", e);
             }
         }
     }
-
-    std::process::exit(0);    
-    // let performance_json = fs::read_to_string(performance_path).expect("Failed to read the file");
-    // // Deserialize the first JSON object in the file
-    // let mut deserializer = Deserializer::from_str(&performance_json);
-    // let results: Vec<Result<Performance>> = deserializer.into_iter().collect();
-    // let connect_result = PgPool::connect(&database_url).await;     
-    // println!("Connect result: {:?}", connect_result);
-    // let pool = connect_result.unwrap();
-    // for result in results {
-    //     match result {
-    //         Ok(performance) => {
-    //             let id_parts: Vec<&str> = performance.benchmark_id.split('/').collect();
-    //             let benchmark_group: &str = id_parts[0];
-    //             println!("Benchmark Group: {:?}", benchmark_group);
-    //             let vector_size: i64 = id_parts[1].parse().unwrap();
-    //             let mut b = meta.clone();
-    //             b.vector_size = vector_size;
-    //             let runtime = normalize_runtime(performance.typical);
-    //             b.runtime_sec = Some(runtime);
-    //             println!("Runtime: {:?}", runtime);
-    //             println!("{:#?}", b);
-    //             let msm_id = 0;
-    //             // let msm_id = zkbench::add_msm(&pool, b).await;
-    //             println!("Added new msm with id {:?}", msm_id);
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error deserializing Performance: {}", e);
-    //         }
-    //     }
-    // }
-    // pool.close().await;
+    pool.close().await;
     Ok(())
 }
